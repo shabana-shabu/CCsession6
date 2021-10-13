6d74702601566ea31992f5ab2f2e0a12af65856d
shabana-shabu <75524151+shabana-shabu@users.noreply.github.com>
2021-10-13 22:30:48 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
-import Grocery from './components/Grocery';
+import'./App.css'
+//import Render from './components/renderlist';
+//import Grocery from './components/Grocery';
+import Form from './components/Form';
  function App() {
   return (
-    <div className="App">
-   <Grocery/>
+    <div className="App"> 
+   {/*<Grocery/>*/}
+  {/*<Render/>*/}
+  <Form/>
     </div>
   );
 }
